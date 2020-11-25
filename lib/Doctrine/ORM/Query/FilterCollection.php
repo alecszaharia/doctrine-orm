0163e2629ec26ef66abe6cd09ff2b3f15528f616
Alex Zaharia <alecszaharia@gmail.com>
2020-11-25 15:56:30 +0200

    Added final getHash method in SQLFilter to avoid caching the filtered queries.
    Issue: https://github.com/doctrine/orm/issues/3955
    
    (cherry picked from commit 398ea442401af41625f290e1e9018e806e38269a)

--- lib/Doctrine/ORM/Query/FilterCollection.php
+++ lib/Doctrine/ORM/Query/FilterCollection.php
@@ -208,7 +208,7 @@ class FilterCollection
         $filterHash = '';
 
         foreach ($this->enabledFilters as $name => $filter) {
-            $filterHash .= $name . $filter;
+            $filterHash .= $name . $filter->getHash();
         }
 
         return $filterHash;
